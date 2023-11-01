a38a1bb2ac91aff2447da95825e40bdd1ae681e6
Andy Wuollet <topcats69@users.noreply.github.com>
2023-10-31 23:58:01 -0500

    add Mary.py

---
+++ Chapter Two/Mary.py
@@ -0,0 +1,10 @@
+name = "Mary"
+password = "swordfish"
+if name == "Mary":
+    print("Hello, Mary")
+    if password == "swordfish":
+        print("Access Granted")
+
+    else:
+        print("Wrong Password")
+
